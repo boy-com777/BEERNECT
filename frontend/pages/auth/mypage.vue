7d480181bce04a02412e23a2d0412eb57a7d2164
boy-com777 <gao.com1208@gmail.com>
2022-04-23 21:29:50 +0900

    フロントエンド：マイページにタブを配置(ユーザーに関わる情報情報をタブで表示する)

--- frontend/pages/auth/mypage.vue
+++ frontend/pages/auth/mypage.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    マイページ
-    <div v-if="user">
-      <p>Email: {{ user.email }}</p>
-      <p>名前: {{ user.name }}</p>
-    </div>
-  </div>
+  <v-app>
+    <v-card
+      width="1280"
+      flat
+      color="transparent"
+      class="mx-auto"
+    >
+      <v-tabs
+        background-color="transparent"
+        grow
+      >
+        <v-tab>
+          プロフィール
+        </v-tab>
+        <v-tab
+          v-if="user"
+        >
+          {{ user.name }} の投稿
+        </v-tab>
+        <v-tab>
+          フォロー
+        </v-tab>
+        <v-tab>
+          フォロワー
+        </v-tab>
+        <v-tab
+          v-if="user"
+        >
+          {{ user.name }} が飲んだビール一覧
+        </v-tab>
+      </v-tabs>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
