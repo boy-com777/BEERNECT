47ac6c2ece57ac2e1a51a129c744adf06fd314e8
boy-com777 <gao.com1208@gmail.com>
2022-05-07 16:16:12 +0900

    フロントエンド：マイページ用のヘッダー作成, tab埋め込みは未実装, 別途対応

---
+++ frontend/layouts/myPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <my-page-app-bar />
+    <v-main>
+      <nuxt />
+    </v-main>
+    <logged-in-footer />
+  </v-app>
+</template>
+
+<script>
+import myPageAppBar from '~/components/loggedIn/myPageAppBar'
+import loggedInFooter from '~/components/loggedIn/loggedInFooter'
+export default {
+  components: {
+    myPageAppBar,
+    loggedInFooter
+  }
+}
+</script>
