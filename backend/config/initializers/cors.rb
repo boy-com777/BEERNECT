5e822dcf0bd9014c4aa227e85470697943167bb4
boy-com777 <gao.com1208@gmail.com>
2022-06-25 18:15:01 +0900

    dockerfile, cors.rbの変数を止めてダイレクト入力

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['DOMAIN'] || ''
+    # origins ENV['DOMAIN'] || ''
+    origins ['localhost:8080']
 
     resource '*',
              headers: :any,
