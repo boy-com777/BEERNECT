af362052a3020a79308825deba9dd1b2cba5578f
boy-com777 <gao.com1208@gmail.com>
2022-07-15 23:57:39 +0900

    全体：本番環境でのAPI_ENDPOINT修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,8 @@
 
 # sudo service nginx start
 cd /app
-bundle exec rails db:seed RAILS_ENV=production
-# bin/setup
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+# bundle exec rails db:seed RAILS_ENV=production
+bin/setup
 # echo $DATABASE_URL
 bundle exec pumactl start
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -57,10 +57,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: '/',
-    baseURL:
-      process.env.NODE_ENV === 'production'
-        ? 'https://backend.beernect.com'
-        : 'http://localhost:3000',
+    baseURL: 'https://backend.beernect.com',
   },
 
   i18n: {
