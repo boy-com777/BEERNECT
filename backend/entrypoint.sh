c85e78628f37e03242aac1609ea44305690d147b
boy-com777 <gao.com1208@gmail.com>
2022-07-16 21:28:26 +0900

    バックエンド：uploader修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,8 @@
 
 # sudo service nginx start
 cd /app
-RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+# RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 # bundle exec rails db:seed RAILS_ENV=production
-bin/setup
+# bin/setup
 # echo $DATABASE_URL
 bundle exec pumactl start
